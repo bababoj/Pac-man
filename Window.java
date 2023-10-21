61dd679ae7c0619181c6c8f28adf25b135d5618a
Arina Nekrasova <98659422+nebababoy@users.noreply.github.com>
2023-10-21 11:15:46 +0200

    c2

--- Window.java
+++ Window.java
@@ -1,55 +1,77 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Window extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Создание окна (Stage)
+
         primaryStage.setTitle("Pacman");
-      //  primaryStage.setWidth(448); // Установите ширину окна в пикселях
-        primaryStage.setWidth(450); // Установите ширину окна в пикселях
-        primaryStage.setHeight(576); // Установите высоту окна в пикселях
         primaryStage.centerOnScreen();
         primaryStage.setResizable(false);
 
-        Group root = new Group();
+        Label label = new Label("Score:     Lives:");
+        label.setStyle("-fx-font-size: 16px; -fx-text-fill: white;");
 
-        int numRows = 31;
-        int numCols = 28;
-        int cellWidth = 16; // Ширина ячейки
-        int cellHeight = 20; // Высота ячейки
+        // Создаем HBox для размещения лейбла по центру
+        HBox topScene = new HBox(label);
+        topScene.setStyle("-fx-background-color: black;");
+        topScene.setAlignment(Pos.CENTER); // Выравнивание по центру по горизонтали и вертикали
+        Scene topSceneObject = new Scene(topScene, 448, 48, Color.BLACK);
 
-        // Создаем GridPane и заполняем его данными
-        GridPane gridPane = new GridPane();
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                Label label = new Label("0"); // Замените "Data" на ваш элемент массива
-                label.setMinWidth(cellWidth);
-                label.setMinHeight(cellHeight);
-                gridPane.add(label, col, row);
+        VBox bottomScene = new VBox();
+        bottomScene.setSpacing(0);
+
+        Maze maze = new Maze();
+
+        int[][] mazeData = maze.getMaze();
+
+        int mazeWidth = mazeData[0].length;
+        int mazeHeight = mazeData.length;
+
+        double cellWidth = 448.0 / mazeWidth;
+        double cellHeight = 496.0 / mazeHeight;
+
+
+        for (int row = 0; row < mazeHeight; row++) {
+            HBox rowBox = new HBox();
+            for (int col = 0; col < mazeWidth; col++) {
+                Rectangle cell = new Rectangle(cellWidth, cellHeight);
+                int cellValue = mazeData[row][col];
+
+                if (cellValue == 0) {
+                    cell.setFill(Color.BLACK);
+
+                } else if (cellValue == 1) {
+                    cell.setFill(Color.BLACK);
+
+                }else if (cellValue == 2){
+                    cell.setFill(Color.BLUE);
+                    // cell.setStyle("-fx-arc-width: 35; -fx-arc-height: 35; -fx-background-color: black;"); // Установка радиуса закругления
+                }
+                  rowBox.getChildren().add(cell);
             }
+            bottomScene.getChildren().add(rowBox);
+
         }
 
-        Scene mainScene = new Scene(gridPane, Color.BLACK);
+        VBox root = new VBox();
+        root.getChildren().addAll(topSceneObject.getRoot(), bottomScene);
 
+        Scene scene = new Scene(root, 448, 521, Color.BLACK);
+        primaryStage.setScene(scene);
 
-
-
-        // Создание контента для окна
-        //Label label = new Label("Привет, мир!");
-        //Scene scene = new Scene(label);
-
-
-        // Установка сцены в окно
-        primaryStage.setScene(mainScene);
-
-        // Отобразить окно
         primaryStage.show();
     }
 }
